39819b25f6e071c8278d0d1f40cf1d1a8860cc5b
hokor <hokoro123@gmail.com>
2021-07-01 17:49:25 +0900

    FIRST PROJECT COMMIT create:base.html , views.py

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_world(request):
-    return HttpResponse('Hello World')
+    return render(request,'base.html') #html 파일을 가져오고 싶을떄
+    #HttpResponse('Hello World')
