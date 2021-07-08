fc0b8b164654682eae5554f3bec70a7c1c289ce5
hokor <hokoro123@gmail.com>
2021-07-08 17:13:34 +0900

    7-8 commit two

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,8 +4,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_world(request):
-    return render(request,'accountapp/hello_world.html') #html 파일을 가져오고 싶을떄
+    #POST/GET 방식에 대한 분기 문 을 작성
+    if request.method == 'POST':
+        return render(request,'accountapp/hello_world.html',context={'text':'POST METHOD'}) #html 파일을 가져오고 싶을떄 context
+    else:
+        return render(request,'accountapp/hello_world.html',context={'text':'GET'})
+
+
     #HttpResponse('Hello World')
+
 '''
 app 안에 template 만들고 accountapp 폴더를 따로 만드는 이유 
 파일만 남겨두면 어디서 가져오는 파일인지 알수가 없다 
