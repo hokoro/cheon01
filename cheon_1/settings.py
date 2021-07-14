7f6525b9843f492fb4383d4fdd122f639c6969ad
hokor <hokoro123@gmail.com>
2021-07-14 12:28:58 +0900

    7-14 commit three

--- cheon_1/settings.py
+++ cheon_1/settings.py
@@ -160,5 +160,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
-LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world') #로그인을 했을시 재연결 시킬
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login') #로그아웃 시킬시 재연결
