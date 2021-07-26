4fce11ef1cd4b94465af29d4dfddf4af1b3beb90
hokoro <hokoro123@gmail.com>
2021-07-26 17:07:49 +0900

    7-26 commit two

--- profileapp/forms.py
+++ profileapp/forms.py
@@ -2,7 +2,7 @@ from django.forms import ModelForm
 
 from profileapp.models import Profile
 
-
+#forms 역할 웹 페이지에서 보여줄 내용의 형태 사용할것인지/안할것인지 형태를 만들어주는게 forms 의 역할이다.
 class ProfileCreationsForm(ModelForm):
     class Meta: #image 에 pixel 이 정보(data) 인데 외적으로 사진을 표현하는게 사진의 촬영날짜 크기 등등 을 meta data 라고 한다.
         model = Profile #profile app 의 model 을 가져온다.
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationsForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile #만들었던 model 가져오기
+    form_class = ProfileCreationsForm
+    success_url = reverse_lazy('accountapp:hello_world') #연결 성공 하면 어디로 연결 account_app 으로 연결가능한것은 app_name 을 찾아서 이다.
+    template_name = 'profileapp/create.html'
+
