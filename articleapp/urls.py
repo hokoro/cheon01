7bd1180d4e9146ef3bc5127cef0710688b3db7e1
hokoro <hokoro123@gmail.com>
2021-07-29 17:59:55 +0900

    7-29 commit four

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,5 +1,7 @@
+from django.urls import path
+from django.views.generic import TemplateView
 
-
+app_name = 'articleapp'
 urlpatterns = [
-
+    path('list/',TemplateView.as_view(template_name='articleapp/list.html'),name = 'list') #템플릿만 가져와서 보여주는 템플릿 뷰를 사용
 ]
