89c0784cf41ba1ca3b5680a747edfdd403252e53
hokoro <hokoro123@gmail.com>
2021-08-04 11:17:27 +0900

    8-4 commit two

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from articleapp.views import ArticleCreateView
+from articleapp.views import ArticleCreateView, ArticleDetailView
 
 app_name = 'articleapp'
 urlpatterns = [
     path('list/',TemplateView.as_view(template_name='articleapp/list.html'),name = 'list'),#템플릿만 가져와서 보여주는 템플릿 뷰를 사용
     path('create/',ArticleCreateView.as_view(),name = 'create'),
+    path('detail/<int:pk>',ArticleDetailView.as_view(),name = 'detail') #주소창에서 pk 가져올 게시글 을 확인 해야 함
 ]
--- articleapp/views.py
+++ articleapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
@@ -17,3 +17,7 @@ class ArticleCreateView(CreateView):
     def form_valid(self, form): #검증이 끝나면 마지막에 실행되는 메소드
         form.instance.writer = self.request.user #글이 작성 되면 요청을 보내는 유저의 정보를 보낸다
         return super().form_valid(form)
+class ArticleDetailView(DetailView):
+    model = Article
+    context_object_name = 'target_article' #접근 하기 위한 패턴
+    template_name = 'articleapp/detail.html'
