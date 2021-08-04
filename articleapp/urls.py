128029e331f72a3058ddedf712dea4b4e23d2c1a
hokoro <hokoro123@gmail.com>
2021-08-04 11:38:22 +0900

    8-4 commit three

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from articleapp.views import ArticleCreateView, ArticleDetailView
+from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView
 
 app_name = 'articleapp'
 urlpatterns = [
     path('list/',TemplateView.as_view(template_name='articleapp/list.html'),name = 'list'),#템플릿만 가져와서 보여주는 템플릿 뷰를 사용
     path('create/',ArticleCreateView.as_view(),name = 'create'),
-    path('detail/<int:pk>',ArticleDetailView.as_view(),name = 'detail') #주소창에서 pk 가져올 게시글 을 확인 해야 함
+    path('detail/<int:pk>',ArticleDetailView.as_view(),name = 'detail'),#주소창에서 pk 가져올 게시글 을 확인 해야 함
+    path('update/<int:pk>',ArticleUpdateView.as_view(),name = 'update'),
 ]
--- articleapp/views.py
+++ articleapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
@@ -11,13 +11,26 @@ from articleapp.models import Article
 class ArticleCreateView(CreateView):
     model = Article
     form_class = ArticleCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    #success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'articleapp/create.html'
 
     def form_valid(self, form): #검증이 끝나면 마지막에 실행되는 메소드
         form.instance.writer = self.request.user #글이 작성 되면 요청을 보내는 유저의 정보를 보낸다
         return super().form_valid(form)
+    def get_success_url(self): #detail page 가 생겼으므로 수정하고 나서 해당 pk 로 등록된 게시글로 가는 방법
+        return reverse('articleapp:detail',kwargs={'pk':self.object.pk})
+
 class ArticleDetailView(DetailView):
     model = Article
     context_object_name = 'target_article' #접근 하기 위한 패턴
     template_name = 'articleapp/detail.html'
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    form_class = ArticleCreationForm
+    context_object_name = 'target_article'
+    #success_url = reverse_lazy('articleapp:detail')
+    template_name = 'articleapp/update.html'
+
+    def get_success_url(self): #detail page 가 생겼으므로 수정하고 나서 해당 pk 로 등록된 게시글로 가는 방법
+        return reverse('articleapp:detail',kwargs={'pk':self.object.pk})
