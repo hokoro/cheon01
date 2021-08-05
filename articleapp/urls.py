f51d51b42491025733461614d5345e1366a4d175
hokoro <hokoro123@gmail.com>
2021-08-05 17:22:23 +0900

    8-5 commit2

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView
+from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView, ArticleListView
 
 app_name = 'articleapp'
 urlpatterns = [
-    path('list/',TemplateView.as_view(template_name='articleapp/list.html'),name = 'list'),#템플릿만 가져와서 보여주는 템플릿 뷰를 사용
+    path('list/',ArticleListView.as_view(),name = 'list'),#템플릿만 가져와서 보여주는 템플릿 뷰를 사용
     path('create/',ArticleCreateView.as_view(),name = 'create'),
     path('detail/<int:pk>',ArticleDetailView.as_view(),name = 'detail'),#주소창에서 pk 가져올 게시글 을 확인 해야 함
     path('update/<int:pk>',ArticleUpdateView.as_view(),name = 'update'),
