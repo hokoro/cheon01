ec8436f7a9a596b3b19d1ecc12030d9c5b348551
hokoro <hokoro123@gmail.com>
2021-08-09 17:58:25 +0900

    8-9 commit5

--- commentapp/views.py
+++ commentapp/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.views.generic import CreateView
 
+from articleapp.models import Article
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
 
@@ -14,3 +15,8 @@ class CommentCreateView(CreateView):
     template_name = 'commentapp/create.html'
     def get_success_url(self):
         return reverse('articleapp:detail',kwargs={'pk':self.object.article.pk}) #원하는 해당 게시글로 보낸다.
+        #할당을 안해줬다..? = 게시글 작성자랑 게시글을 아직 못받아서 데이터가 없다 db 에도 현재 null 로 초기화
+    def form_valid(self, form):
+        form.instance.writer = self.request.user #로그인 하고 해야함
+        form.instance.article_id = self.request.POST.get('article_pk')
+        return super().form_valid(form)
