bc0e8ef0f0f81e3605dce840a548a2d92f9a7d04
hokoro <hokoro123@gmail.com>
2021-08-12 17:15:36 +0900

    8-12 commit three

--- cheon_1/urls.py
+++ cheon_1/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('profile/',include('profileapp.urls')),
     path('article/',include('articleapp.urls')),
     path('comment/',include('commentapp.urls')),
+    path('project/',include('projectapp.urls')),
 ] +static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT) #미디어를 요청했을떄 알려줘야 한다. URL 과 ROOT 를
