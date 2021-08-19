9d0ae6765fd40ecc0b31eabf6b72614195ad0248
hokoro <hokoro123@gmail.com>
2021-08-19 17:59:57 +0900

    8-19 commit five

--- subscribeapp/models.py
+++ subscribeapp/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='subscription',null = False) #유저가 탈퇴하면 구독정보도 같이 삭제 한다
+
+    project =models.ForeignKey(Project,on_delete=models.CASCADE,related_name='subscription',null=False) #게시판이 삭제되면 구독 정보도 삭제
+
+    class Meta:
+        unique_together = ['user','project'] #유저와 프로젝트는 한개의 쌍으로 한개만 존재하게 설정 한다
