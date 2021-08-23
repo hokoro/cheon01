e7e0662be669a9f38ab1fc0da215eb56f8a60757
hokoro <hokoro123@gmail.com>
2021-08-23 17:20:06 +0900

    8-23 commit two

--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -4,8 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, ListView
 
+from articleapp.models import Article
 from projectapp.models import Project
 from subscribeapp.models import Subscription
 
@@ -28,3 +29,21 @@ class SubscriptionView(RedirectView):
 
     def get_redirect_url(self, *args, **kwargs):
         return reverse('projectapp:detail',kwargs={'pk': kwargs['project_pk']})
+
+@method_decorator(login_required,'get')
+class SubscriptionListView(ListView):
+    #게시글
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'subscribeapp/list.html'
+    paginate_by = 20
+
+    #get context data 는 templates 의 모든 데이터를 조종 가능 할수 있다
+    # 어떤 게시글을 볼건지 queryset 어떤 게시글 을 볼건지만 지정할수 있음 요소 하나만 사용
+    def get_queryset(self):
+        # 지금 접속한 유저의 모든 구독 정보를 가져온다 + 구독 모델에 안에 있는 project 만 가져옴 = values_list method
+        project_list = Subscription.objects.filter(user = self.request.user).values_list('project')
+        #그 프로젝트 안에 있는 게시글만 filter 를 시킨다.
+        article_list = Article.objects.filter(project__in=project_list)
+
+        return article_list
