ef6375e5164d86e0d7b61ed0616f8cd9514340f4
hokoro <hokoro123@gmail.com>
2021-08-26 16:56:11 +0900

    8-26 commit one

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,9 +1,14 @@
+from django import forms
 from django.forms import ModelForm
 
 from articleapp.models import Article
 
 
 class ArticleCreationForm(ModelForm):
-    class Meta: #외부 정보를 얻기 위해서 받는 meta class
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable',
+                                                           'style': 'min-height:10rem;'
+                                                                    'text-align: left;'}))  # forms 안에 charfield 를 사용하여 class customizing 을 한다  textarea 라는 widget 을 사용한다.
+
+    class Meta:  # 외부 정보를 얻기 위해서 받는 meta class
         model = Article
-        fields = ['title','project','image','content']
+        fields = ['title', 'project', 'image', 'content']
